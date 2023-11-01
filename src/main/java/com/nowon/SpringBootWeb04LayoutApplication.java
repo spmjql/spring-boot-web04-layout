329e61c67aef87e70227bc7f2d090f7a07dcbac8
Administrator <Administrator@304-28>
2023-11-01 14:31:53 +0900

    sdklsfdlajkl

---
+++ src/main/java/com/nowon/SpringBootWeb04LayoutApplication.java
@@ -0,0 +1,13 @@
+package com.nowon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeb04LayoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWeb04LayoutApplication.class, args);
+	}
+
+}
